234a718660a1ed301a02685d57a08d1bbfda4871
WaluigiTime <43816974+WaluigiTime@users.noreply.github.com>
2021-01-18 17:53:04 -0500

    day 1 steering wheel fell off a mile back

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,15 @@ import data from "../sample_data.json";
 
 function App() {
   return <div className="app">Trivia!</div>;
-}
+  return <div className = "Question value">
+    "Question goes here"</div>
 
 export default App;
+
+let Question = 0;
+
+<Question value= "Question goes here"/>
+
+
+
+
--- src/index.js
+++ src/index.js
@@ -5,4 +5,6 @@ import App from "./components/App";
 
 // render the App in to the DOM inside the element with id "root"
 ReactDOM.render(<App />, document.getElementById("root"));
-// sa
+// WOAH YANKEE NO BRIM
+// WOAH YANKEE 360 BRIM
+// WOAH NO YANKEE NO BRIM
