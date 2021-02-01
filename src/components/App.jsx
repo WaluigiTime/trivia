a558cdfb9c2ae8d6f1af10f56b8d341e994f8b49
WaluigiTime <43816974+WaluigiTime@users.noreply.github.com>
2021-02-01 17:48:27 -0500

    this is a funny button something about buttons? we didn't finish nextquestion rendering lolllll

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,16 +3,26 @@ import "../css/App.css";
 import data from "../sample_data.json";
 
 function App() {
-  return <div className="app">Trivia!</div>;
-  return <div className = "Question value">
-    "Question goes here"</div>
+  let questionNumber = 0;
+  return (
+    <div className="app">
+      Trivia
+      <Question value={data[questionNumber].question.text} />
+      <NextQuestion />
+    </div>
+  );
+}
+
+function Question(props) {
+  return <div className="question">Question: {props.value}</div>;
+}
+
+function NextQuestion() {
+  return (
+    <div className="nextquestion">
+      <button>This is a funny button</button>
+    </div>
+  );
+}
 
 export default App;
-
-let Question = 0;
-
-<Question value= "Question goes here"/>
-
-
-
-
