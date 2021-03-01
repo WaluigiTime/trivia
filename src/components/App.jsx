bfd15c49f38210fd801e1003be294faa6dc40848
RagedHavoc <48684216+RagedHavoc@users.noreply.github.com>
2021-03-01 17:59:44 -0500

    Did #39 in READ ME

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,17 +17,19 @@ function Question(props) {
   return (
     <div className="question">
       Question: {props.data[props.questionNumber].question.text}
-      <Answer data={props.data} questionNumber={props.questionNumber} />
+      <Answer
+        data={props.data}
+        text={props.data[props.questionNumber].question.choices[0]}
+      />
+      <Answer text={props.data[props.questionNumber].question.choices[1]} />
+      <Answer text={props.data[props.questionNumber].question.choices[2]} />
+      <Answer text={props.data[props.questionNumber].question.choices[3]} />
     </div>
   );
 }
 
 function Answer(props) {
-  return (
-    <div className="answer">
-      Answer: {props.data[props.questionNumber].question.correct_choice_index}{" "}
-    </div>
-  );
+  return <div className="answer">Choice: {props.text}</div>;
 }
 
 function NextQuestion() {
